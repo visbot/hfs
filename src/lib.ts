c2c173f458c5caf79cc4ef44d98ab64555a02657
Jan T. Sott <git@idleberg.com>
2024-01-02 15:18:01 +0100

    export library

---
+++ src/lib.ts
@@ -0,0 +1,4 @@
+export { add } from './actions/add';
+export { init } from './actions/init';
+export { remove } from './actions/remove';
+export { serve } from './actions/serve';
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-	target: 'esnext',
-  clean: true,
-  dts: true,
-  entry: ['src/cli.ts'],
-  format: 'esm',
-  minify: true,
-	outDir: 'bin',
-  sourcemap: process.env.NODE_ENV !== 'production',
-  treeshake: 'recommended'
-});
+export default defineConfig([
+	{
+		target: 'esnext',
+		clean: true,
+		dts: true,
+		format: 'esm',
+		minify: true,
+		outDir: 'bin',
+		sourcemap: process.env.NODE_ENV !== 'production',
+		treeshake: 'recommended'
+	}
+]);
